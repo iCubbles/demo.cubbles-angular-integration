eead0666f72bfcd773498b4a5f17ea88ef5a0995
Philipp Wagner <philipp.wagner@incowia.com>
2016-05-18 13:24:54 +0200

    PLAT-480: adjust second demo in demo2.html by extending on site documentation

--- public/js/demo2.js
+++ public/js/demo2.js
@@ -3,6 +3,7 @@
 
   var app = angular.module('angularCubbles', []);
 
+  // create directive cubx-component
   app.directive('cubxComponent', [function() {
     return {
       restrict : 'E',
@@ -15,6 +16,7 @@
     };
   }]);
 
+  // create controller
   app.controller('mainCtrl', ['$scope', function($scope) {
     $scope.rangeValue = 50;
     $scope.cubxRoot = 'https://cubbles.world/sandbox/com.incowia.demo.travel-planner@0.2.0-SNAPSHOT/co2-footprint/main';
@@ -29,20 +31,22 @@
     });
   }]);
 
+  // link function is called each time the directive cubx-component is instantiated
   var linkCubxComponent = function($scope, element) {
     // get local reference to Client Runtime Container (CRC)
     var CRC = window.cubx.CRC;
-    // build custom event for starting bootstrap of CIF
+    // build custom event for starting bootstrap of CIF (here, use the deprecated way that also works in IE)
     var event = document.createEvent('CustomEvent');
     event.initCustomEvent('CubxComponentLinked', true, true, {});
 
 
-    // when CIF is ready set slot values given to directive via attribute "slotValues"
+    // when CIF is ready set slot values given to directive via scope property "slotValues"
     document.addEventListener('cifReady', function() {
-      // initially cal setSlots when cif is ready
+      // initially call setSlots when cif is ready
       setSlots($scope, element);
 
-      // register watcher for all slots available in $scope.slotValues object
+      // register watcher for all slots available in $scope.slotValues object.
+      // Here we only have a single input slot named "value". The iteration below could be part of a more generic solution
       for(var slotName in $scope.slotValues) {
         if ($scope.slotValues.hasOwnProperty(slotName)) {
           $scope.$watch('slotValues.' + slotName + '.value', function(newValue, oldValue) {
@@ -52,10 +56,11 @@
       };
     });
 
-    // dispatch event
+    // dispatch 'CubxComponentLinked' event
     document.dispatchEvent(event);
   };
 
+  // set slot of cubbles component instance 'c2-footprint'
   var setSlots = function($scope, element) {
     var cubbleCompInstance = element.find('[cubx-dependency]')[0];
 
